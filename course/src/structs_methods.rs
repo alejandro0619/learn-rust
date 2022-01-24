23489d11697dddaa80530d66f13e313a20a542ab
alejandro Lopez <alejandrolpz0619@gmail.com>
2022-01-24 15:09:40 -0400

    🚀Changes: course/src/structs_methods.rs

--- course/src/structs_methods.rs
+++ course/src/structs_methods.rs
@@ -14,7 +14,25 @@ impl Rectangle {
   fn display(&self)  {
     println!("{:?}", &self);
   }
+
+  fn can_hold(&self, other_rect: &Rectangle) -> bool {
+    self.w > other_rect.w && self.h > other_rect.h
+  }
+  // Associated functions:
+  // Associated functions are methods of the struct
+  // But we can create associated functions that doesn't have &self as the first parameter
+  // And thus they are not methods because it does not need the instance of the struct
+  // We already used one called String::from() which doesn't require to be called from an instance but from the type itself
+  fn square(size: i32) -> Rectangle {
+    Rectangle {
+      w: size,
+      h: size
+    }
+  }
 }
+
+
+
 pub fn start(){
   let rect1 = Rectangle {
     w: 5,
@@ -22,4 +40,7 @@ pub fn start(){
   };
   rect1.display();
   rect1.area();
+  let can_hold: bool = rect1.can_hold( &Rectangle{ w: 5, h:3 } );
+  // call the associated function withput &self param
+  let rect2 = Rectangle::square(21);
 }
